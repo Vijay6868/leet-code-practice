4927abe881d106a247bac81cad19660486a7eea0
Vijay6868 <saroyav3@gmail.com>
2023-11-06 10:50:23 +1300

    27 solved

---
+++ 27-Remove-Element/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _27_Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[] nums1 = { 3, 2, 2, 3 };
+            int val1 = 2;
+            //Console.WriteLine(nums1);
+            Console.WriteLine(RemoveElement(nums1, val1));
+            
+        }
+        public static int RemoveElement(int[]nums, int val)
+        {
+            int result = 0;
+            foreach (int item in nums)
+            {
+                if (item != val)
+                {
+                    nums[result] = item;
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
