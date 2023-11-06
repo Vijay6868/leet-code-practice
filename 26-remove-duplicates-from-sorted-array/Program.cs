e1745f4e04eed763bcbc89b07a10ca45a364923f
Vijay6868 <saroyav3@gmail.com>
2023-11-06 14:33:18 +1300

    26 solved

--- 26-remove-duplicates-from-sorted-array/Program.cs
+++ 26-remove-duplicates-from-sorted-array/Program.cs
@@ -6,7 +6,24 @@ namespace _26_remove_duplicates_from_sorted_array
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            Console.WriteLine(RemoveDuplicates(nums));
+        }
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int temp = 0;
+            for (int i=0; i < nums.Length-1; i++)
+            {
+                
+                if (nums[i] != nums[i + 1])
+                {
+                    nums[temp] = nums[i];
+                    temp++;
+                }
+            }
+            
+            nums[temp] = nums[nums.Length-1];
+            return temp+1;
         }
     }
 }
