98d4ac1cd9392d849c4192971bc2431d48d5cec0
Vijay6868 <saroyav3@gmail.com>
2023-11-07 21:43:12 +1300

    working on 80

--- 80-remove-duplicates-from-sorted-2/Program.cs
+++ 80-remove-duplicates-from-sorted-2/Program.cs
@@ -12,12 +12,18 @@ namespace _80_remove_duplicates_from_sorted_2
         public static int removeDuplicates(int[] nums)
         {
             int count = 0;
+            int count2 = 0;
             
             for(int i=0; i < nums.Length-1; i++)
             {
                 if (nums[i] != nums[i + 1])
                 {
-                    nums[count] = nums[i];
+                    if (count2 == 1)
+                    {
+                        nums[count] = nums[i];
+                            count2 = 0;
+                    }
+                    
 
                     count++;
                 }
