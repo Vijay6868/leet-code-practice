8759f668d8fce18a80d436813b2cfbc4e702ae2a
Vijay6868 <saroyav3@gmail.com>
2023-11-07 21:35:32 +1300

    working on 80

--- 80-remove-duplicates-from-sorted-2/Program.cs
+++ 80-remove-duplicates-from-sorted-2/Program.cs
@@ -6,7 +6,30 @@ namespace _80_remove_duplicates_from_sorted_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 1, 1, 2, 2, 3 };
+            Console.WriteLine(removeDuplicates(arr));
+        }
+        public static int removeDuplicates(int[] nums)
+        {
+            int count = 0;
+            
+            for(int i=0; i < nums.Length-1; i++)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    nums[count] = nums[i];
+
+                    count++;
+                }
+
+            }
+            nums[count] = nums[nums.Length - 1];
+
+            foreach(int element in nums)
+            {
+                Console.WriteLine(element);
+            }
+            return count+1;
         }
     }
 }
