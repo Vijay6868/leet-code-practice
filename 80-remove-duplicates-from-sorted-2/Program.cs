dea3fcdf0f0c5592cb5de9b328f212bc5b340ff9
Vijay6868 <saroyav3@gmail.com>
2023-11-07 21:03:15 +1300

    started working on 80

---
+++ 80-remove-duplicates-from-sorted-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _80_remove_duplicates_from_sorted_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
