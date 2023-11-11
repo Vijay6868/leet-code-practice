2b8d5ae6dcb5c738c59eefc80cb543974d7a7713
Vijay6868 <saroyav3@gmail.com>
2023-11-11 16:31:55 +1300

    working on 169

--- 169-Majority-Element/Program.cs
+++ 169-Majority-Element/Program.cs
@@ -6,7 +6,31 @@ namespace _169_Majority_Element
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 2,3,3 };
+
+            Console.WriteLine(MajortiyElement(arr));
+        }
+        public static int MajortiyElement(int[] nums)
+        {
+            int count = 0, collector = 0, majEle = nums[0];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > collector)
+                {
+                    majEle = nums[i];
+                }
+                collector = count;
+                count = 0;
+                
+            }
+            return majEle;
         }
     }
 }
