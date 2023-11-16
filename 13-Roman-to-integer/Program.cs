f95bc7213d4745c3ce360e2d9728e9bde46a98cc
Vijay6868 <saroyav3@gmail.com>
2023-11-16 22:06:46 +1300

    working on 13

--- 13-Roman-to-integer/Program.cs
+++ 13-Roman-to-integer/Program.cs
@@ -6,7 +6,31 @@ namespace _13_Roman_to_integer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string value = "LVIII";
+            Console.WriteLine(RomanToInt(value));
+
+        }
+        public static int RomanToInt(string s)
+        {
+            int counter = 0;
+
+            for(int i=s.Length-1; i >=0; i--)
+            {
+                if (s[i] == 'I')
+                {
+                    counter = counter + 1;
+                }
+                else if (s[i] == 'V')
+                {
+                    counter = counter + 5;
+                }
+                else if (s[i] == 'L')
+                {
+                    counter = counter + 50;
+                }
+            }
+
+            return counter;
         }
     }
 }
