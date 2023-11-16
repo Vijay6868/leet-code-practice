b858f0364205a292e5d478c8129ce1fe6c9f733e
Vijay6868 <saroyav3@gmail.com>
2023-11-16 23:11:21 +1300

    58 added

---
+++ 58-Length-of-Last-String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _58_Length_of_Last_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
