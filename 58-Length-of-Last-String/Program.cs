c15e78e273da88725a6e53bae66d6f99f3d7b201
Vijay6868 <saroyav3@gmail.com>
2023-11-16 23:28:28 +1300

    finished-58

--- 58-Length-of-Last-String/Program.cs
+++ 58-Length-of-Last-String/Program.cs
@@ -6,7 +6,26 @@ namespace _58_Length_of_Last_String
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = "Hello World";
+            Console.WriteLine(LengthOfLastWord(s));
+        }
+        public static int LengthOfLastWord(string s)
+        {
+            int length = 0;
+
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                if(s[i]==' ' && length>0)
+                {
+                    break;
+                }
+                else if(s[i]!=' ')
+                {
+                    length += 1;
+                }
+            }
+            return length;
         }
     }
+
 }
