b1c287cb339483f4094534853a05d2bf2d44548e
Vijay Kumar <109253143+Vijay6868@users.noreply.github.com>
2023-11-18 23:22:35 +1300

    14 Added but not solved yet

---
+++ 14-Longest-Common-Prefix/14-Longest-Common-Prefix.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       int[] nums = {2,3,1,1,4};
+    }
+     public string LongestCommonPrefix(string[] strs) {
+        
+    }
+}
