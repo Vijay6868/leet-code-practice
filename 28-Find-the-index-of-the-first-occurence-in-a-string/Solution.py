4cbf32c8b00c981f69e8ff94ea6ae9c76ada3825
Vijay6868 <saroyav3@gmail.com>
2023-11-25 13:42:05 +1300

    28 partially solved -1

--- 28-Find-the-index-of-the-first-occurence-in-a-string/Solution.py
+++ 28-Find-the-index-of-the-first-occurence-in-a-string/Solution.py
@@ -1,21 +1,27 @@
 
 def strStr(haystack, needle):
     j=0
+    index=0
     for i in range(0,len(haystack)):
         if needle[j]==haystack[i]:
+        
            if j<len(needle)-1:
             j = j+1
            else:
               break
+        if j==1:
+            index=i
+            
+               
         #elif j>0 and j != len(needle):
            # j=0
-
+    print(j)
     if j==len(needle)-1:
-        return j
-    else:
+        return index
+    elif j!=len(needle)-1:
         return -1
 
 haystack = "leetcode"
 needle = "leeto"
-
+print(len(needle))
 print(strStr(haystack,needle))
