6ee4901b640f3a44be907dccd774a3c75635df5a
Vijay6868 <saroyav3@gmail.com>
2024-01-19 09:25:15 +1300

    case5 solved

---
+++ 28-Find-the-index-of-the-first-occurence-in-a-string/case6.py
@@ -0,0 +1,26 @@
+def strStr1(haystack, needle):
+   nchar=needle[0]
+
+   for i in range(0,len(haystack)):
+            if haystack[i] == nchar and len(haystack)>=len(needle):
+                count=1
+                idx=i+1
+                for x in range(1,len(needle)):
+                    print(needle[x]+" nx "+haystack[idx]+" hx")
+                    if needle[x]==haystack[idx]:
+                        count+=1
+                    idx+=1
+                #print("value of count "+ str(count) )       
+                if count==len(needle):
+                    #count=0
+                    #print("value of i : "+str(i))
+                     
+                     return i
+                #else:
+                     #return-1
+            elif i==len(haystack)-1:
+                 return -1
+haystack="mississippi"
+needle= "issip"       
+print(strStr1(haystack,needle))
+#print(len(needle))
